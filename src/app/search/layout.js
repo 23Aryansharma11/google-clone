7984a4e4a1483a557dc43d92e6e51f99f290fb51
Aryan Sharma <23aryansharma11@gmail.com>
2024-04-17 22:38:10 +0530

    fix: layout

--- src/app/search/layout.js
+++ src/app/search/layout.js
@@ -1,10 +1,13 @@
 import SearchHeader from "C/SearchHeader"
 import './../globals.css'
 import { Toaster } from "react-hot-toast"
+import { Suspense } from "react"
 const layout = ( { children } ) => {
   return (
     <div>
+      <Suspense>
         <SearchHeader />
+      </Suspense>
       { children }
       <Toaster toastOptions={
       {duration: 700,}
